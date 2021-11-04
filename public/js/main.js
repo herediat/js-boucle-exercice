d1cbf007356769c85b226fbe2ab41c79be49db7a
herediat <ts.heredia@outlook.com>
2021-11-04 20:01:01 +0100

    First saving

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+// ## Exercice 1 - Boucles Foreach
+
+// ## Créer un tableau dans le quel vous devez avoir les prenoms de chaque student de la Coding School 15
+// let coding = ["Albert", "Amir", "Assma", "Bryan", "Florence", "Hugo", "Kevin", "Marcelin", "Martin", "Mustafa", "Paul", "Rayane", "Theodora", "Violette", "L'Autre", "Younes B"];
+
+
+// // ## A l'aide l'un foreach, affichez la valeur de chaque element et la position
+// coding.forEach((e, i) => {
+//     console.log(`Element -> ${e}\n Position -> ${i}`);
+// });
+
+
+
+// ## Exercice 2 - Boucles (Foreach)
+
+// ## Créer un tableau dans le quel vous devez avoir les prenoms de chaque student de la Coding School 15
+// let coding = ["Albert", "Amir", "Assma", "Bryan", "Florence", "Hugo", "Kevin", "Marcelin", "Martin", "Mustafa", "Paul", "Rayane", "Theodora", "Violette", "L'Autre", "Younes B"];
+
+// // // ## A l'aide l'un foreach, affichez les prenom des studens qui commenecent par (A, F, G, I, H, L, M) en majuscule, les autres en minuscule
+
+// coding.forEach( e => {
+//     if (e.charAt(0) == ("A") || e.charAt(0) == ("F") || e.charAt(0) == ("G") || e.charAt(0) == ("I")
+//     || e.charAt(0) == ("H") || e.charAt(0) == ("L") || e.charAt(0) == ("M")) {
+//         console.log(`${e.toUpperCase()}`);
+//     } else {
+//         console.log(`${e.toLowerCase()}`);
+//     };
+// });
